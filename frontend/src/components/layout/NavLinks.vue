1e11185259234d0e3226ecc4b62c3480ed1aab2d
Peter Epp <peter.epp@icloud.com>
2026-04-08 00:56:32 -0600

    style: improved mobile nav layout/sizing

--- frontend/src/components/layout/NavLinks.vue
+++ frontend/src/components/layout/NavLinks.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router'
+
+const props = withDefaults(defineProps<{ size?: 'small' | 'large' }>(), { size: 'small' })
 </script>
 
 <template>
-  <RouterLink to="/dashboard" class="nav-link"><i class="pi pi-gauge text-xs" />Dashboard</RouterLink>
-  <RouterLink to="/timeline" class="nav-link"><i class="pi pi-chart-line text-xs" />Timeline</RouterLink>
-  <RouterLink to="/domains-report" class="nav-link"><i class="pi pi-globe text-xs" />Domains</RouterLink>
-  <RouterLink to="/devices-report" class="nav-link"><i class="pi pi-mobile text-xs" />Devices</RouterLink>
+  <RouterLink to="/dashboard" :class="['nav-link', props.size === 'large' && 'nav-link-large']"><i :class="['pi pi-gauge', props.size === 'large' ? 'text-sm' : 'text-xs']" />Dashboard</RouterLink>
+  <RouterLink to="/timeline" :class="['nav-link', props.size === 'large' && 'nav-link-large']"><i :class="['pi pi-chart-line', props.size === 'large' ? 'text-sm' : 'text-xs']" />Timeline</RouterLink>
+  <RouterLink to="/domains-report" :class="['nav-link', props.size === 'large' && 'nav-link-large']"><i :class="['pi pi-globe', props.size === 'large' ? 'text-sm' : 'text-xs']" />Domains</RouterLink>
+  <RouterLink to="/devices-report" :class="['nav-link', props.size === 'large' && 'nav-link-large']"><i :class="['pi pi-mobile', props.size === 'large' ? 'text-sm' : 'text-xs']" />Devices</RouterLink>
 </template>
